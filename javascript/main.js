b0d79d3b65f1523d041482a2c4f256de914ae3aa
aracelicaes <aracelicaes@gmail.com>
2021-03-04 14:44:41 -0600

    Add Functions to Main.js Add createBook() Add displayBook() Add library div in index.html

--- javascript/main.js
+++ javascript/main.js
@@ -8,3 +8,39 @@ function Books(title, author, pages) {
 
 const book = new Books('Harry Potter', 'JK Rowling', 400)
 console.log(book)
+console.log(book.title)
+console.log(book.author)
+myLibrary.push(book);
+
+const book2 = new Books('Lolita', 'Samba', 600)
+console.log(book2)
+myLibrary.push(book2);
+
+console.log(myLibrary);
+
+function createBook(){
+  let titleInput = document.getElementById('title').value;
+  let authorInput = document.getElementById('author').value;
+  let pagesInput = document.getElementById('pages').value;
+
+  let newBook = new Books(titleInput, authorInput, pagesInput);
+  myLibrary.push(newBook);
+}
+
+function displayBook() {
+  myLibrary.forEach((book) => {
+    let container = document.getElementById('library');
+    let bookDiv = document.createElement('div');
+    let titleDisplay = document.createElement('p');
+    let authorDisplay = document.createElement('p');
+    let pagesDisplay = document.createElement('p');
+    
+    
+
+  })
+}
+
+
+
+
+
