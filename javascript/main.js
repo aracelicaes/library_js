99cc1a12199db7c9aaca8b1a6bf4fde3ae7fa299
janis-jenny <pcardenasvm@gmail.com>
2021-03-05 11:00:04 -0500

    move event handler to remove books and add validations

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,18 @@
 let myLibrary = [];
 
-function Books(title, author, pages) {
+function Book(title, author, pages) {
   this.title = title
   this.author = author
   this.pages = pages
 }
 
-const book = new Books('Harry Potter', 'JK Rowling', 400)
+const book = new Book('Harry Potter', 'JK Rowling', 400)
 console.log(book)
 console.log(book.title)
 console.log(book.author)
 myLibrary.push(book);
 
-const book2 = new Books('Lolita', 'Samba', 600)
+const book2 = new Book('Lolita', 'Samba', 600)
 console.log(book2)
 myLibrary.push(book2);
 
@@ -22,16 +22,18 @@ function createBook() {
   let titleInput = document.getElementById('title').value;
   let authorInput = document.getElementById('author').value;
   let pagesInput = document.getElementById('pages').value;
-
   let newBook = new Books(titleInput, authorInput, pagesInput);
+  if (titleInput.length > 1 && authorInput.length > 1 && pagesInput > 0) {
   myLibrary.push(newBook);
+  }
 }
 
 function displayBook() {
+  let container = document.getElementById('library'); //Toreset the container everytime we display a new book 
+  container.innerHTML = ''
   myLibrary.forEach((book, index) => {
-    let container = document.getElementById('library');
     let bookDiv = document.createElement('div');
-    bookDiv.setAttribute('id', index);
+    bookDiv.setAttribute('id', index); // add an atribute of its corresponded index
     let titleDisplay = document.createElement('p');
     let authorDisplay = document.createElement('p');
     let pagesDisplay = document.createElement('p');
@@ -49,15 +51,24 @@ function displayBook() {
     bookDiv.appendChild(pagesDisplay);
     bookDiv.appendChild(removeButton);
   })
-}
 
+  let removeBtn = document.querySelectorAll('.removeBtn'); // to return all of the removeBtn elements in a page
+  removeBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+    const book = e.target.parentNode
+    book.remove()
+    })
+  })
+}
+/* 
 function removeBook() {
   // get book from HTML getElementById(#)
   // abstract action to remove book
   // array [{id:0}, {id: 1}, {id:2}]
+  document.getAttribute()
   let book = document.getElementById(1);
-  console.log(book);
-}
+  console.log(book); */
+
 
 
 const btn = document.querySelector('#btn');
@@ -68,11 +79,5 @@ btn.addEventListener('click', (e) => {
   displayBook();
 });
 
-let removeBtn = document.querySelector('.removeBtn');
-if (removeBtn) {
-  removeBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    alert("You sure you wanna remove me????");
-    // removeBook();
-  })
-}
+
+console.log(removeBtn);
