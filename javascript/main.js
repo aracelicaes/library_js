f9ccb9a87e8cfba3e98ffc31c3746bddfcdc09c3
aracelicaes <aracelicaes@gmail.com>
2021-03-05 10:48:48 -0600

    Add Status to Book Object Initialize Status in Book{} Get Status From Drop Down Add Select Menu for Status in HTML Add Status Display in displayBook()

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,18 @@
 let myLibrary = [];
+window.myLibrary = myLibrary;
 
-function Book(title, author, pages) {
+function Book(title, author, pages, status) {
   this.title = title
   this.author = author
   this.pages = pages
+  this.status = status
 }
 
-const book = new Book('Harry Potter', 'JK Rowling', 400)
+const book = new Book('Harry Potter', 'JK Rowling', 400, 'Read')
 console.log(book)
-console.log(book.title)
-console.log(book.author)
 myLibrary.push(book);
 
-const book2 = new Book('Lolita', 'Samba', 600)
+const book2 = new Book('Lolita', 'Samba', 600, 'Not Read')
 console.log(book2)
 myLibrary.push(book2);
 
@@ -22,11 +22,14 @@ function createBook() {
   let titleInput = document.getElementById('title').value;
   let authorInput = document.getElementById('author').value;
   let pagesInput = document.getElementById('pages').value;
-  let newBook = new Book(titleInput, authorInput, pagesInput);
+  let statusInput = document.getElementById('status').value;
+  console.log(statusInput)
+  let newBook = new Book(titleInput, authorInput, pagesInput, statusInput);
   if (titleInput.length > 1 && authorInput.length > 1 && pagesInput > 0) {
   myLibrary.push(newBook);
   }
 }
+
 
 function displayBook() {
   let container = document.getElementById('library'); //Toreset the container everytime we display a new book 
@@ -37,6 +40,7 @@ function displayBook() {
     let titleDisplay = document.createElement('p');
     let authorDisplay = document.createElement('p');
     let pagesDisplay = document.createElement('p');
+    let statusDisplay = document.createElement('p');
     let removeButton = document.createElement('button');
     removeButton.innerText = 'Remove Book';
     removeButton.setAttribute('class', 'removeBtn');
@@ -44,11 +48,18 @@ function displayBook() {
     titleDisplay.innerText = book.title
     authorDisplay.innerText = book.author
     pagesDisplay.innerText = book.pages
+    console.log(book.status)
+    if (book.status === "read") {
+      statusDisplay.innerText = 'Read';
+    } else {
+      statusDisplay.innerText = 'Unread';
+    }
 
     container.appendChild(bookDiv);
     bookDiv.appendChild(titleDisplay);
     bookDiv.appendChild(authorDisplay);
     bookDiv.appendChild(pagesDisplay);
+    bookDiv.appendChild(statusDisplay);
     bookDiv.appendChild(removeButton);
   })
 
@@ -60,24 +71,11 @@ function displayBook() {
     })
   })
 }
-/* 
-function removeBook() {
-  // get book from HTML getElementById(#)
-  // abstract action to remove book
-  // array [{id:0}, {id: 1}, {id:2}]
-  document.getAttribute()
-  let book = document.getElementById(1);
-  console.log(book); */
-
 
 
 const btn = document.querySelector('#btn');
 btn.addEventListener('click', (e) => {
-  // alert("Hello World");
   e.preventDefault()
   createBook();
   displayBook();
 });
-
-
-console.log(removeBtn);
