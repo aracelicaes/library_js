8ba816417e5cf39eaa7b7031be317b12da3550c2
janis-jenny <pcardenasvm@gmail.com>
2021-03-10 11:10:43 -0500

    fix eslint linters

--- javascript/main.js
+++ javascript/main.js
@@ -2,122 +2,6 @@ const myLibrary = [];
 window.myLibrary = myLibrary;
 let counter = 0;
 
-function Book(title, author, pages, status) {
-  this.id = counter;
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
-  counter++;
-}
-
-
-function createBook() {
-  let titleInput = document.getElementById('title').value;
-  let authorInput = document.getElementById('author').value;
-  let pagesInput = document.getElementById('pages').value;
-  let statusInput = document.getElementById('status').value;
-  document.querySelector('form').reset();
-  let newBook = new Book(titleInput, authorInput, pagesInput, statusInput);
-  if (titleInput.length > 1 && authorInput.length > 1 && pagesInput > 0) {
-    myLibrary.push(newBook);
-  }
-  console.log(myLibrary)
-  setLocalStorage();
-}
-
-function displayBook() {
-  let container = document.getElementById('library'); //To reset the container everytime we display a new book 
-  container.innerHTML = ''
-  myLibrary.forEach((book) => {
-    let bookDiv = document.createElement('div');
-    bookDiv.setAttribute('id', book.id); // the id is the lenght of the library and -1 becuase i want to start from index 0
-    let titleDisplay = document.createElement('p');
-    let titleName = document.createElement('p');
-    let authorDisplay = document.createElement('p');
-    let authorName = document.createElement('p');
-    let pagesDisplay = document.createElement('p');
-    let pagesNumber = document.createElement('p');
-    let buttonsDiv = document.createElement('div');
-    let statusDisplay = document.createElement('button');
-    let removeButton = document.createElement('button');
-    removeButton.innerText = 'Delete';
-    buttonsDiv.setAttribute('class', 'buttons-div')
-    removeButton.setAttribute('class', 'removeBtn');
-    statusDisplay.setAttribute('class', 'status-btn'); //the id is uniwue so to be the same as the id from bookDiv we set it in data-id attribute, porque no deberia haber 2 elementos con el mismo id
-    removeButton.setAttribute('data-id', book.id); // my button has to hav the smae id of bookDiv and to no use id we use th data-id which is an atribute of html
-    titleDisplay.innerText = book.title                     // data-es un atributo de datos.
-    authorDisplay.innerText = book.author
-    pagesDisplay.innerText = book.pages
-    titleName.innerText = 'Title:'                        // data-es un atributo de datos.
-    authorName.innerText = 'Author:'
-    pagesNumber.innerText = 'Pages:'
-    if (book.status === 'read') {
-      statusDisplay.innerText = 'Read';
-    } else {
-      statusDisplay.innerText = 'Unread';
-    }
-    container.appendChild(bookDiv);
-    bookDiv.appendChild(titleName);
-    bookDiv.appendChild(titleDisplay);
-    bookDiv.appendChild(authorName);
-    bookDiv.appendChild(authorDisplay);
-    bookDiv.appendChild(pagesNumber);
-    bookDiv.appendChild(pagesDisplay);
-    bookDiv.appendChild(buttonsDiv);
-    buttonsDiv.appendChild(statusDisplay);
-    buttonsDiv.appendChild(removeButton);
-  })
-  deleteBtn();
-  changeStatus();
-}
-
-const bookForm = () => {
-  const theForm = document.getElementById("theForm");
-  if (theForm.style.display === "none" || theForm.style.display === '') {
-    theForm.style.display = "block";
-  } else {
-    theForm.style.display = "none";
-  }
-}
-
-const deleteBtn = () => {
-  const removeBtn = document.querySelectorAll('.removeBtn'); // to return all of the removeBtn elements in a page
-  // console.log(removeBtn)
-  removeBtn.forEach((btn) => {
-    btn.addEventListener('click', (e) => {      //.e.target: returns the element that triggered the event, va al boton donde hice click y me da el valor del data-id = X con dataset.od
-      myLibrary.splice(e.target.dataset.id - 1, 1) //estoy eliminando el boton con el id = x del array
-      // console.log(myLibrary)
-      displayBook()
-    })
-    setLocalStorage();
-  })
-}
-
-const getStatusValue = (e) => {
-  e.target.innerHTML.toUpperCase() === 'READ'
-    ? e.target.innerHTML = 'Unread'
-    : e.target.innerHTML = 'Read'
-}
-
-const changeStatus = () => {
-  const statusBtn = document.querySelectorAll('.status-btn');
-  statusBtn.forEach(btn => {
-    btn.addEventListener('click', e => {
-      getStatusValue(e);
-    })
-    setLocalStorage();
-  })
-}
-
-
-const btn = document.querySelector('#btn');
-btn.addEventListener('click', (e) => {
-  e.preventDefault();
-  createBook();
-  displayBook();
-});
-
 const setLocalStorage = () => {
   window.localStorage.setItem('library', JSON.stringify(myLibrary));
 };
@@ -133,6 +17,122 @@ const getLocalStorage = () => {
     counter = myLibrary[last].id + 1;
   }
 };
+
+function Book(title, author, pages, status) {
+  this.id = counter;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
+  counter += 1;
+}
+
+function createBook() {
+  const titleInput = document.getElementById('title').value;
+  const authorInput = document.getElementById('author').value;
+  const pagesInput = document.getElementById('pages').value;
+  const statusInput = document.getElementById('status').value;
+  document.querySelector('form').reset();
+  const newBook = new Book(titleInput, authorInput, pagesInput, statusInput);
+  if (titleInput.length > 1 && authorInput.length > 1 && pagesInput > 0) {
+    myLibrary.push(newBook);
+  }
+  setLocalStorage();
+}
+
+
+const getStatusValue = (e) => {
+  if (e.target.innerHTML.toUpperCase() === 'READ') {
+    e.target.innerHTML = 'Unread';
+  } else {
+    e.target.innerHTML = 'Read';
+  }
+};
+
+const changeStatus = () => {
+  const statusBtn = document.querySelectorAll('.status-btn');
+  statusBtn.forEach(btn => {
+    btn.addEventListener('click', e => {
+      getStatusValue(e);
+    });
+    setLocalStorage();
+  });
+};
+
+function displayBook() {
+  const container = document.getElementById('library'); // To reset the container everytime we display a new book
+  container.innerHTML = '';
+  myLibrary.forEach((book) => {
+    const bookDiv = document.createElement('div');
+    bookDiv.setAttribute('id', book.id); // the id is the lenght of the library and -1 becuase i want to start from index 0
+    const titleDisplay = document.createElement('p');
+    const titleName = document.createElement('p');
+    const authorDisplay = document.createElement('p');
+    const authorName = document.createElement('p');
+    const pagesDisplay = document.createElement('p');
+    const pagesNumber = document.createElement('p');
+    const buttonsDiv = document.createElement('div');
+    const statusDisplay = document.createElement('button');
+    const removeButton = document.createElement('button');
+    removeButton.innerText = 'Delete';
+    buttonsDiv.setAttribute('class', 'buttons-div');
+    removeButton.setAttribute('class', 'removeBtn');
+    statusDisplay.setAttribute('class', 'status-btn');
+    removeButton.setAttribute('data-id', book.id);
+    titleDisplay.innerText = book.title;
+    authorDisplay.innerText = book.author;
+    pagesDisplay.innerText = book.pages;
+    titleName.innerText = 'Title:';
+    authorName.innerText = 'Author:';
+    pagesNumber.innerText = 'Pages:';
+    if (book.status === 'read') {
+      statusDisplay.innerText = 'Read';
+    } else {
+      statusDisplay.innerText = 'Unread';
+    }
+    container.appendChild(bookDiv);
+    bookDiv.appendChild(titleName);
+    bookDiv.appendChild(titleDisplay);
+    bookDiv.appendChild(authorName);
+    bookDiv.appendChild(authorDisplay);
+    bookDiv.appendChild(pagesNumber);
+    bookDiv.appendChild(pagesDisplay);
+    bookDiv.appendChild(buttonsDiv);
+    buttonsDiv.appendChild(statusDisplay);
+    buttonsDiv.appendChild(removeButton);
+  });
+  deleteBtn();
+  changeStatus();
+}
+
+const deleteBtn = () => {
+  const removeBtn = document.querySelectorAll('.removeBtn'); // to return all of the removeBtn elements in a page
+  removeBtn.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      myLibrary.splice(e.target.dataset.id - 1, 1);
+      displayBook();
+    });
+    setLocalStorage();
+  });
+};
+
+const bookForm = () => {
+  const theForm = document.getElementById('theForm');
+  if (theForm.style.display === 'none' || theForm.style.display === '') {
+    theForm.style.display = 'block';
+  } else {
+    theForm.style.display = 'none';
+  }
+};
+
+bookForm();
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', (e) => {
+  e.preventDefault();
+  createBook();
+  displayBook();
+});
 
 window.addEventListener('DOMContentLoaded', () => {
   getLocalStorage();
